607aabd072b71045f7be3da6d24d95e6fe85e9ca
IdoFolk <121632753+IdoFolk@users.noreply.github.com>
2024-01-26 14:20:00 +0200

    grapple mechanic prototype

--- Hooker3D/Assets/Grappler.cs
+++ Hooker3D/Assets/Grappler.cs
@@ -5,51 +5,66 @@ using UnityEngine;
 
 public class Grappler : MonoBehaviour
 {
+    [SerializeField] private Camera mainCamera;
     [SerializeField] private Rigidbody2D rigidBody;
+    [SerializeField] private DistanceJoint2D distanceJoint2D;
     [SerializeField] private Transform firePoint;
     [SerializeField] private LineRenderer lineRenderer;
     [SerializeField] private float grappleDistance;
+    [SerializeField] private Player player;
+    [SerializeField, Range(0,1)] private float grapplePullDistanceRatio;
+    [SerializeField] private float angularForce;
+    
 
-    [SerializeField]private SpringJoint2D springJoint2D;
-    private bool _isGrappling;
 
     private void Start()
     {
-        springJoint2D.enabled = false;
+        distanceJoint2D.enabled = false;
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        int inputKey = 0;
+        switch (player)
+        {
+            case Player.Player1:
+                inputKey = 0;
+                break;
+            case Player.Player2:
+                inputKey = 1;
+                break;
+        }
+        if (Input.GetMouseButtonDown(inputKey))
         {
             Grapple();
         }
-        if (Input.GetMouseButtonUp(0))
+        else if (Input.GetMouseButtonUp(inputKey))
         {
-            _isGrappling = false;
+            distanceJoint2D.enabled = false;
+            lineRenderer.enabled = false;
         }
 
-        if (_isGrappling)
+        if (distanceJoint2D.enabled)
         {
-            springJoint2D.anchor = firePoint.position;
-            lineRenderer.positionCount = 2;
-            lineRenderer.SetPosition(0,springJoint2D.anchor);
-            lineRenderer.SetPosition(1,springJoint2D.connectedAnchor);
+            lineRenderer.SetPosition(1,transform.position);
         }
+
+       
     }
 
     private void Grapple()
     {
-        RaycastHit2D hit = Physics2D.Raycast(firePoint.position, Vector2.right);
-        Debug.DrawRay(firePoint.position,Vector2.right);
+        RaycastHit2D hit = Physics2D.Raycast(firePoint.position, firePoint.right,grappleDistance);
         if (hit.collider != null)
         {
-            springJoint2D.connectedAnchor = hit.point;
-            springJoint2D.distance = 0;
-            springJoint2D.enabled = true;
-            _isGrappling = true;
+            lineRenderer.SetPosition(0, hit.point);
+            lineRenderer.SetPosition(1, transform.position);
+            distanceJoint2D.connectedAnchor = hit.point;
+            distanceJoint2D.distance = Vector3.Distance(hit.point, transform.position) * grapplePullDistanceRatio;
+            rigidBody.angularDrag = angularForce;
+            distanceJoint2D.enabled = true;
+            lineRenderer.enabled = true;
         }
-        
     }
 
     private void OnDrawGizmos()
@@ -60,4 +75,10 @@ public class Grappler : MonoBehaviour
         }
         
     }
+}
+
+public enum Player
+{
+    Player1,
+    Player2
 }
