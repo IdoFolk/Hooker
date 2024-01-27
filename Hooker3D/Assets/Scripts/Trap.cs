6dfebe5908e57859a6f127079b566ea7e60312ad
IdoFolk <121632753+IdoFolk@users.noreply.github.com>
2024-01-27 15:47:46 +0200

    .

--- Hooker3D/Assets/Scripts/Trap.cs
+++ Hooker3D/Assets/Scripts/Trap.cs
@@ -6,6 +6,8 @@ public class Trap : MonoBehaviour
     {
         if (collision != null)
         {
+            var spaceship = collision.gameObject.GetComponent<Spaceship>();
+            if (spaceship is not null) spaceship.DeathSFX();
             CheckPoint.SpawnAtLastSpawnPoint(collision.gameObject);
             collision.gameObject.GetComponent<Rigidbody2D>().totalForce = Vector2.zero;
         }
