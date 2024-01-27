2a98b7f53c8bff803378d7d82936a794efb7befc
EDeyal <75429849+EDeyal@users.noreply.github.com>
2024-01-27 15:33:03 +0200

    Win game

--- Hooker3D/Assets/Scripts/WinGame.cs
+++ Hooker3D/Assets/Scripts/WinGame.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 public class WinGame : MonoBehaviour
 {
     [SerializeField] Canvas _canvas;
+    GameObject _spaceship;
+    bool _moveIntoHole;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision != null)
         {
-            RollCredits();
             var spaceship = collision.GetComponent<Spaceship>();
+            _spaceship = spaceship.gameObject;
             spaceship.enabled = false;
+            _moveIntoHole = true;
+            _spaceship.GetComponent<Rigidbody2D>().gravityScale = 0;
+            StartCoroutine(Wait2Seconds());
+        }
+    }
+    IEnumerator Wait2Seconds()
+    {
+        yield return new WaitForSeconds(2);
+        RollCredits();
+    }
+    private void FixedUpdate()
+    {
+        if (_moveIntoHole)
+        {
+            Vector2 forcedirection = new Vector2(this.transform.position.x, this.transform.position.y) - new Vector2(_spaceship.transform.position.x, _spaceship.transform.position.y);
+            _spaceship.GetComponent<Rigidbody2D>().AddForce(forcedirection*20);
         }
     }
     private void RollCredits()
